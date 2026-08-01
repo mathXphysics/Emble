64fd88a598a34e31cab0efe3da773aa66bdea713
Malte Freiherr <malte.freiherr@outlook.de>
2026-08-01 11:15:00 +0200

    gui compatible with bitboards

--- uci.py
+++ uci.py
@@ -72,7 +72,7 @@ def uci_loop():
         command = input().strip()
 
         if command == "uci":
-            print("id name Emble 5.02", flush=True)
+            print("id name Emble 6.0", flush=True)
             print("id author Malte Freiherr", flush=True)
             print("uciok", flush=True)
 
